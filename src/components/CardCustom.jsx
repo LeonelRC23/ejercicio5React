cfcbf678dc2fa77f6607859bbce92ab335fc4cb8
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2024-02-01 14:19:33 -0300

    Implementacion del localstorage

--- src/components/CardCustom.jsx
+++ src/components/CardCustom.jsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/cardCustom.css';
 import Task from './Task';
 
 const CardCustom = () => {
-  const [tarea, setTarea] = useState([]);
+  const tareaLS = JSON.parse(localStorage.getItem('tareaKey')) || [];
+  const [tarea, setTarea] = useState(tareaLS);
   const onKeyEnter = (e) => {
     if (e.key == 'Enter') {
       if (e.target.value.trim().length !== 0) {
         let object;
         if (tarea.length == 0) {
-           object = {
+          object = {
             id: 1,
             desc: e.target.value.trim(),
           };
         } else {
-           object = {
+          object = {
             id: tarea[tarea.length - 1].id + 1,
             desc: e.target.value.trim(),
           };
@@ -25,6 +26,13 @@ const CardCustom = () => {
       }
     }
   };
+  let deleteElement = (id) => {
+    let tareaAUX = [...tarea].filter((elm) => elm.id !== id);
+    setTarea(tareaAUX);
+  };
+  useEffect(() => {
+    localStorage.setItem('tareaKey', JSON.stringify(tarea));
+  }, [tarea]);
   return (
     <div className='d-flex justify-content-center align-items-center min-vh-100'>
       <div className='content p-5'>
@@ -36,7 +44,11 @@ const CardCustom = () => {
         <div>
           <ul>
             {tarea.map((element) => (
-              <Task task={element.desc} key={element.id} />
+              <Task
+                task={element}
+                key={element.id}
+                deleteElement={deleteElement}
+              />
             ))}
           </ul>
         </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const Task = ({ task }) => {
-  let deletElement = (e) => {
-    //como actualizo el componente para que borre la tarea seleccionada
-  }
+const Task = ({ task, deleteElement }) => {
   return (
     <li className='d-flex justify-content-between my-2'>
-      {task}
-      <Button className='btn-light' onClick={deletElement}>x</Button>
+      {task.desc}
+      <Button className='btn-light' onClick={() => deleteElement(task.id)}>
+        x
+      </Button>
     </li>
   );
 };
